f69e32e49ed526799c3c8878aa2a1fd6bfa538d0
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-12 16:51:48 +0700

    feat: add secret element

--- src/Components/LearnState/Click.tsx
+++ src/Components/LearnState/Click.tsx
@@ -6,7 +6,9 @@ export const Click = () => {
    const [value, setValue] = useState(0);
 
    useEffect(() => {
-      document.title = `Sudah Click sebanyak ${value}`;
+      value > 0
+         ? (document.title = `Sudah Click sebanyak ${value}`)
+         : (document.title = "Halo . . .");
    });
 
    return (
@@ -15,6 +17,11 @@ export const Click = () => {
          <p>Silahkan Di-Click</p>
          <p>
             Kamu sudah mengeklik sebanyak: <span>{value}</span>
+         </p>
+         <p className='secret'>
+            {value === 10 || value === 20
+               ? `Selamat kamu Sudah mengeklik sebanyak ${value}`
+               : ""}
          </p>
          <Buttons
             className='btn'
