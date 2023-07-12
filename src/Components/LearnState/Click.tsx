bae8b9abbd366499315c2f6d1560012b89665db7
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-12 17:19:48 +0700

    feat: add logic FE

--- src/Components/LearnState/Click.tsx
+++ src/Components/LearnState/Click.tsx
@@ -9,6 +9,17 @@ export const Click = () => {
       value > 0
          ? (document.title = `Sudah Click sebanyak ${value}`)
          : (document.title = "Halo . . .");
+
+      const secret: any = document.querySelector(".secret");
+      value === 10 || value === 20
+         ? secret.classList.add("see")
+         : secret.classList.add("hidden");
+
+      value !== 10 && value !== 20
+         ? secret.classList.remove("see")
+         : secret.classList.add("hidden");
+
+      console.log(secret);
    });
 
    return (
@@ -18,10 +29,11 @@ export const Click = () => {
          <p>
             Kamu sudah mengeklik sebanyak: <span>{value}</span>
          </p>
-         <p className='secret'>
+         <p className='secret '>
             {value === 10 || value === 20
-               ? `Selamat kamu Sudah mengeklik sebanyak ${value}`
+               ? `Selamat kamu Sudah mengeklik sebanyak `
                : ""}
+            <span>{value === 10 || value === 20 ? `${value}` : ""}</span>
          </p>
          <Buttons
             className='btn'
